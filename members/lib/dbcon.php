d5e56bd698c8fee75a73b606d9a5c0f9d00c04e8
KwasiEvans <73921854+KwasiEvans@users.noreply.github.com>
2021-06-30 13:11:58 +0000

    Church

---
+++ members/lib/dbcon.php
@@ -0,0 +1,30 @@
+<?php
+//core
+$conn;
+function dbcon(){
+	global $conn;
+	$user = "root";
+	$pass = "";
+	$host = "localhost";
+	$db = "cman";
+	$conn = mysqli_connect($host,$user,$pass,$db) or die("cannot connect to the database");
+}
+
+function host(){
+	$h = "http://".$_SERVER['HTTP_HOST']."/bankdb/";
+	return $h;
+}
+
+function hRoot(){
+	$url = $_SERVER['DOCUMENT_ROOT']."/bankdb/";
+	return $url;
+}
+
+//parse string
+function gstr(){
+    $qstr = $_SERVER['QUERY_STRING'];
+    parse_str($qstr,$dstr);
+    return $dstr;
+}
+
+?>
